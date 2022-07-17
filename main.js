060f2b2e5cede54ee0e9d453142d040e96b91159
William Blair <w.blair@lancaster.ac.uk>
2022-07-17 23:55:36 +0100

    Add game, getWinner & incrementPoints functions

--- main.js
+++ main.js
@@ -57,3 +57,45 @@ function playRound(playerSelection, computerSelection)
   console.log(outcome);
   return winner;
 }
+
+function incrementPoints(winner, points)
+{
+  console.log(points[0]);
+  if(winner === "Player")
+    points[0]++;
+  else if(winner === "Computer")
+    points[1]++;
+  console.log("SCORE: Player " + points[0] + " - Computer: " + points[1]);
+}
+
+function getWinner(points)
+{
+  if(points[0] === 3)
+    return "Player";
+  else
+    return "Computer";
+}
+
+function game()
+{
+  const choices = ["Rock", "Paper", "Scissors"];
+  let computerSelection;
+  let playerSelection;
+  let roundWinner;
+  let gameWinner;
+  let points = [0, 0];
+
+  while(/*player*/points[0] < 3 && /*computer*/points[1] < 3)
+  {
+    playerSelection = prompt("Enter your choice: ");
+    computerSelection = getComputerChoice(choices);
+    roundWinner = playRound(playerSelection, computerSelection);
+    incrementPoints()
+  }
+
+  gameWinner = getWinner(points);
+  console.log("The " + gameWinner + " wins!");
+  console.log("FINAL SCORE: Player " + points[0] + " - Computer: " + points[1]);
+}
+
+game();
